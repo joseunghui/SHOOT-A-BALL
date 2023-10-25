a2ab1d1d897265dac2ac229a5df6e6551cbc542f
zelda <zelda@plgrim.com>
2023-10-25 15:59:14 +0900

    test

--- server/index.js
+++ server/index.js
@@ -12,14 +12,68 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-    socket.on('hi', (info) => { //info 정보를 받아서 
-        io.emit('hi', `Hi user ${info}`); // 모든 유저에게 `Hi user ${info}` 메시지 보내기
-        console.log(socket.id); // 서버 확인
-    })
-    
-})
-
 server.listen(3000, () => {
     console.log(`Example app listening on 3000`)
+});
+
+// map 을 사용해서 데이터 저장
+const userSocketId = new Map();
+const wordMap = new Map();
+
+// 랜덤 초성 생성
+function randomChosung() 
+{
+    const word = [
+        "ㄱ",
+        "ㄴ",
+        "ㄷ",
+        "ㄹ",
+        "ㅁ",
+        "ㅂ",
+        "ㅅ",
+        "ㅇ",
+        "ㅈ",
+        "ㅊ",
+        "ㅋ",
+        "ㅌ",
+        "ㅍ",
+        "ㅎ",
+      ];
+      
+      function randomChosung() {
+        const chosung = [
+          word[Math.ceil(Math.random() * word.length) - 1],
+          word[Math.ceil(Math.random() * word.length) - 1],
+        ];
+        return chosung.join("");
+      }
+}
+
+io.on('connection', (socket) => {
+
+    socket.on("join", (userId) => {
+        // 2p 게임
+        // size가 0이면 user1을 저장하고 아니면 user2로 저장
+        if (userSocketIdMap.size === 0) {
+          userSocketIdMap.set("user1", userId);
+          io.emit("join", `hi user1 ${userId}`);
+        } else {
+          userSocketIdMap.set("user2", userId);
+          io.emit("join", `hi user2 ${userId}`);
+        }
+        console.log(userSocketIdMap.keys(), userId);
+    });
+
+    // user1과 user2가 들어왔으면 wordMap에 randomWord를 세팅(randomChosung() 에서 생성한 단어)
+    socket.on("start", () => {
+        if (userSocketIdMap.size === 2) {
+          const randomWord = randomChosung(); // randomChosung() 로 초성 세팅
+          wordMap.set("word", randomWord);
+          io.emit("start", randomWord);
+        }
+    });
+
+    socket.on("message", ({ fromId, message }) => {
+        io.emit("message", { fromId, message });
+    });
 });
